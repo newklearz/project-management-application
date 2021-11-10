eaa9b2ee976071885281656a9efb941ec65958d2
newklearz <thezayene@gmail.com>
2021-11-10 14:22:07 +0200

    #6 : Create login page in Angular      Create login component      Create auth component for future routing of Login, Register and ForgetPassword

--- project-management-application-web/src/app/app.component.ts
+++ project-management-application-web/src/app/app.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app';
-  title2= 'eu-angular-app';
 }
--- project-management-application-web/src/app/app.module.ts
+++ project-management-application-web/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
