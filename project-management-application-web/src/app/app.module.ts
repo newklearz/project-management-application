47ab8a025f68752322c0418d173050eabf31184f
newklearz <thezayene@gmail.com>
2021-11-18 15:20:37 +0200

    #9 : Create admin page in Angular

--- project-management-application-web/src/app/app.module.ts
+++ project-management-application-web/src/app/app.module.ts
@@ -9,13 +9,15 @@ import {ErrorPageComponent} from "./error-page/error-page.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {XhrInterceptor} from "./xhr.interceptor";
+import {HeaderComponent} from "./header/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ErrorPageComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
